811e371c5ed285fa192fc04e588b8bb482093aff
lianglin.lynch <lianglin.lynch@bytedance.com>
2020-07-17 20:08:30 +0800

    feat: 爬虫学习

--- code/spider.chinanews.com.clean.py
+++ code/spider.chinanews.com.clean.py
@@ -5,7 +5,7 @@ Created on Sat Apr  4 17:38:38 2020
 @author: Zhenlin
 """
 
-#清理乱码新闻
+# 清理乱码新闻
 
 import os
 import xml.etree.ElementTree as ET
@@ -14,28 +14,28 @@ import configparser
 config = configparser.ConfigParser()
 config.read('../config.ini', 'utf-8')
 
-folder=config['DEFAULT']['doc_dir_path']
+folder = config['DEFAULT']['doc_dir_path']
 
-files=os.listdir(folder)
+files = os.listdir(folder)
 
 files = sorted(files, key=lambda x: int(os.path.splitext(x)[0]))
 
-keyword='编辑'
+keyword = '编辑'
 
-i=1
-for j,file in enumerate(files):
-    print('%d/%d'%(j,len(files)))
-    path='%s\%s'%(folder,file)
+i = 1
+for j, file in enumerate(files):
+    print('%d/%d' % (j, len(files)))
+    path = '%s\%s' % (folder, file)
 
-    tree =  ET.parse(path)
+    tree = ET.parse(path)
     root = tree.getroot()
     body = root.find('body').text
     docid = root.find('id')
     if keyword in body:
         docid.text = str(i)
-        tree.write('%s\%d.xml'%(folder,i), encoding=config['DEFAULT']['doc_encoding'])
-        i+=1
+        tree.write('%s\%d.xml' % (folder, i), encoding=config['DEFAULT']['doc_encoding'])
+        i += 1
     else:
         os.remove(path)
-        
-print('# valid files = %d'%(i-1))
+
+print('# valid files = %d' % (i - 1))
